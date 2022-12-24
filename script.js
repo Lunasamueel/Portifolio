869db48f323131f22dc00598a78b06c5815c7b14
Lunasamueel <san.samueel@gmail.com>
2022-12-24 15:18:19 -0300

    portifolio completo

---
+++ script.js
@@ -0,0 +1,13 @@
+window.sr = ScrollReveal({reset: true})
+
+sr.reveal(".experiencia", {duration: 2000})
+sr.reveal(".empresa", {duration: 2000})
+sr.reveal(".empresa-habilidades", {duration: 2000})
+sr.reveal(".formacao", {duration: 2000})
+sr.reveal(".footer", {duration: 2000})
+
+
+
+
+
+
